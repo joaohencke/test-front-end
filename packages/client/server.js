04a95e09ad6d3929166eda314565cdb6341b1e7c
Joao Hencke <joao.hencke@gmail.com>
2020-09-13 19:35:52 -0300

    feat: docker

--- packages/client/server.js
+++ packages/client/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`App is running at port: ${PORT}`)
+  console.log(`App is running at port: ${PORT}`);
 });
